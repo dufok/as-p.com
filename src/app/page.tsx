4b09d28739a7cc15d0dab889ddc3c68ee9912335
Stepan Vladovskiy <duffokin@gmail.com>
2023-11-24 22:09:34 +0000

    create: push to github

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center bg-gradient-to-t from-black to-white">
+      <h1 className="text-5xl font-bold text-white">asáp.com</h1>
+    </main>
+  )
+}
